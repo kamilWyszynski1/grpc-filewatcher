91cb046ee4b16532334fb7ea40f010e2c5de2db2
Kamil Wyszynski <kamil.wyszynski@thunes.com>
2021-08-11 21:32:08 +0200

    bolt db & listener for changes

--- server/main.go
+++ server/main.go
@@ -7,30 +7,47 @@ import (
 	"os"
 
 	"github.com/kamilWyszynski1/filewatcher-grpc/internal/lru"
-	wtch "github.com/kamilWyszynski1/filewatcher-grpc/internal/watcher"
+	pb "github.com/kamilWyszynski1/filewatcher-grpc/internal/watcher"
+	bolt "go.etcd.io/bbolt"
 	"google.golang.org/grpc"
 )
 
+const (
+	address  = "0.0.0.0:50051"
+	boltPath = "./bolt.db"
+)
+
 func main() {
-	address := "0.0.0.0:50051"
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalf("Error %v", err)
 	}
 	fmt.Printf("Server is listening on %v ...", address)
 
+	db, err := bolt.Open(boltPath, 0666, nil)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	changes := make(chan *pb.Change)
+
 	cache := lru.NewLRU(10, nil)
-	svc := wtch.NewService(cache)
+	repo := pb.NewRepository(db)
+	if err := repo.StartListening(changes); err != nil {
+		panic(err)
+	}
+	svc := pb.NewService(cache, repo)
 	err = svc.
 		Logger(log.New(os.Stdout, "", log.LstdFlags)).
-		Filename("../internal/testdata/testfile.txt").
-		Watch()
+		Filename("internal/testdata/testfile.txt").
+		Watch(changes)
 	if err != nil {
 		panic(err)
 	}
 
 	server := grpc.NewServer()
-	wtch.RegisterWatcherServiceServer(server, svc)
+	pb.RegisterWatcherServiceServer(server, svc)
 	if err := server.Serve(lis); err != nil {
 		panic(err)
 	}
